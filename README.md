422072fe8ad80adfd840558c64572609b9ca393c
xsoma <somageller06@gmail.com>
2021-12-05 18:40:24 +0100

    +_-

---
+++ README.md
@@ -0,0 +1,27 @@
+# Saphire - Free opensource sdk cheat based on legendware
+
++ Changes
+  + Ragebot
+    + Improved aimbot
+    + Improved hitchace 
+    + Improved resolver
+    + Improved animation system 
+    + Reworked doubletap
+    
+  + Visuals
+    + Added new menu
+    + Added imgui render 
+    + Added blur by postprocessing 
+    + Added watermark selections
+    + Added player avatars in spectator list
+    
+  + Miscs
+    + Changed clantag
+    + Changed clientstate
+    + Reworked sdk
+    + Added cl_move hook
+    + Removed L3D451R7 antipaste
+    + Reworked setupbones
+
+    
+  
---
+++ constchars.h
@@ -0,0 +1,573 @@
+#pragma once
+const char* watermark_adder[] =
+{
+	"Username",
+	"Server IP",
+	"Ping",
+	"Time",
+	"FPS"
+};
+
+const char* AimType[] =
+{
+	"Hitbox",
+	"Nearest hitbox"
+};
+
+const char* tracer_mode[] =
+{
+	"Off",
+	"Line",
+	"Beam"
+};
+
+const char* LegitHitbox[] =
+{
+	"Head",
+	"Neck",
+	"Pelvis",
+	"Stomach",
+	"Lower chest",
+	"Chest",
+	"Upper chest"
+};
+
+const char* LegitSelection[] =
+{
+	"Near crosshair",
+	"Lowest health",
+	"Highest damage",
+	"Lowest distance"
+};
+
+const char* antiaim_type[] =
+{
+	"Rage AA",
+	"Legit AA"
+};
+
+const char* movement_type[] =
+{
+	"Stand",
+	"Slow walk",
+	"Move",
+	"Air"
+};
+
+const char* LegitFov[] =
+{
+	"Static",
+	"Dynamic"
+};
+
+const char* LegitSmooth[] =
+{
+	"Static",
+	"Humanized"
+};
+
+const char* RCSType[] =
+{
+	"Always on",
+	"On target"
+};
+
+const char* selection[] =
+{
+	"Cycle",
+	"Near crosshair",
+	"Lowest distance",
+	"Lowest health",
+	"Highest damage"
+};
+
+const char* bodyaimlevel[] =
+{
+	"Low",
+	"Medium",
+	"High"
+};
+
+const char* bodyaim[] =
+{
+	"In air",
+	"On high velocity",
+	"On lethal",
+	"On double tap",
+	"If unresolved",
+	"Prefer"
+};
+
+const char* autostop_modifiers[] =
+{
+	"Between shots",
+	"If lethal",
+	"Only Visible",
+	"Only Center",
+	"Force accuracy",
+	"Early"
+};
+
+const char* cond[] =
+{
+	"Lethal",
+	"Prefer",
+	"Enemy in air",
+	"Resolver unsure"
+};
+
+const char* hitboxes[] =
+{
+	"Head",
+	"Upper chest",
+	"Chest",
+	"Lower chest",
+	"Stomach",
+	"Pelvis",
+	"Arms",
+	"Legs",
+	"Feet"
+};
+
+const char* pitch[] =
+{
+	"None",
+	"Down",
+	"Up",
+	"Zero",
+	"Jitter",
+	"Fake down",
+	"Fake up",
+	"Fake jitter",
+	"Custom",
+};
+
+const char* yaw[] =
+{
+	"Off",
+	"Jitter",
+	"Spin",
+
+};
+const char* yaw_add[] =
+{
+	"Off",
+	"Forward",
+	"Backward",
+	"Right",
+	"Left",
+	"Left/Right Jitter",
+	"Zero/Back Jitter",
+	"Backward Jitter",
+	"Custom"
+};
+
+
+const char* fs_mode[] =
+{
+	"Yaw",
+	"Desync side",
+	"Yaw",
+	"Desync side",
+	"Yaw",
+	"Desync side",
+	"Yaw",
+	"Desync side",
+	"Yaw",
+	"Desync side",
+	"Yaw",
+	"Desync side",
+};
+
+const char* baseangle[] =
+{
+	
+	"Crosshair",
+	"Enemy",
+};
+
+const char* desync[] =
+{
+	"None",
+	"Static",
+	"Jitter"
+};
+
+const char* lby_type[] =
+{
+	"Normal",
+	"Opposite",
+	"Sway",
+};
+
+const char* timer_type[] =
+{
+	"Circular",
+	"Rectangle"
+};
+
+const char* proj_combo[] =
+{
+	"Icon",
+	"Text",
+	"Box",
+	"Glow"
+};
+
+const char* weaponplayer[] =
+{
+	"Icon",
+	"Text"
+};
+
+const char* weaponesp[] =
+{
+	"Icon",
+	"Text",
+	"Box",
+	"Distance",
+	"Glow",
+	"Ammo"
+};
+const char* safe_if[] =
+{
+	"Lethal",
+	"Prefer"
+};
+
+const char* baim_if[] =
+{
+	"Lethal",
+	"Prefer",
+	"Resolver unsure"
+
+};
+
+const char* hitmarkers[] =
+{
+	"At crosshair",
+	"At hitted enemy"
+};
+
+const char* glowtarget[] =
+{
+	"Enemy",
+	"Teammate",
+	"Local"
+};
+
+const char* glowtype[] =
+{
+	"Standard",
+	"Pulse",
+	"Inner"
+};
+const char* beamtype[] =
+{
+	"Beam",
+	"Line",
+
+};
+const char* enemy_chams_type[] =
+{
+	"Visible",
+	"Hidden",
+	"Backtrack",
+	"Ragdoll",
+	"Shot",
+};
+
+const char* local_chams_type[] =
+{
+	"Real",
+	"Fake",
+	"Arms",
+	"Weapon"
+};
+
+const char* chams_layer[] =
+{
+	"Main",
+	"Second"
+};
+
+const char* chamsvisact[] =
+{
+	"Visible",
+	"Hidden"
+};
+
+const char* chamsvis[] =
+{
+	"Visible"
+};
+const char* cl_tag[] =
+{
+	"Off",
+	"twotick",
+	"Custom"
+};
+const char* dm_type[] =
+{
+	"Contour",
+	"Wide contour"
+};
+const char* mm_funk[] =
+{
+	"Unlock inventory access",
+	"Spoof sv_pure" ,
+	"Rank reveal",
+	"Auto accept"
+};
+const char* chamstype[] =
+{
+	"Default",
+	"Metal",
+	"Flat",
+	"Pulse",
+	"Crystal",
+	"Glass",
+	"Contour",
+	"Wide contour",
+	"Animated",
+	"Velvet",
+	"Glow Wireframe"
+};
+const char* flags[] =
+{
+	"Money",
+	"Armor",
+	"Have kits",
+	"In scope",
+	"Fakeduck",
+	"Hittable",
+	"Flashed",
+	"Exploit",
+	"Delay",
+	"Bomb carrier"
+};
+
+const char* removals[] =
+{
+	"Scope",
+	"Zoom",
+	"Smoke",
+	"Flash",
+	"Recoil",
+	"Landing bob",
+	"Post processing",
+	"Fog"
+};
+
+const char* indicators[] =
+{
+	"Fake",
+	"Desync side",
+	"Choke",
+	"Damage override",
+	"Safe points",
+	"Body aim",
+	"Double tap",
+	"Hide shots"
+};
+
+const char* skybox[] =
+{
+	"None",
+	"Tibet",
+	"Baggage",
+	"Italy",
+	"Aztec",
+	"Vertigo",
+	"Daylight",
+	"Daylight 2",
+	"Clouds",
+	"Clouds 2",
+	"Gray",
+	"Clear",
+	"Canals",
+	"Cobblestone",
+	"Assault",
+	"Clouds dark",
+	"Night",
+	"Night 2",
+	"Night flat",
+	"Dusty",
+	"Rainy",
+	"Custom"
+};
+
+const char* mainwep[] =
+{
+	"None",
+	"Auto",
+	"AWP",
+	"Scout"
+};
+const char* dt_mode[] =
+{
+	"Default",
+	"Rapid"
+};
+const char* p_angs_v[] =
+{
+	"Down",
+	"Up",
+	"Zero"
+};
+const char* p_angs_r[] =
+{
+	"Up(if zero)",
+	"Down(if zero)",
+	"Down(if up)",
+	"Up(if down)",
+};
+const char* secwep[] =
+{
+	"None",
+	"Dual Berettas",
+	"Deagle/Revolver"
+};
+
+const char* bbhh[] =
+{
+	"None",
+	"Default",
+	"Hitchanced"
+
+};
+
+const char* strafes[] =
+{
+	"None",
+	"Legit",
+	"Directional",
+	"Rage"
+};
+
+const char* events_output[] =
+{
+	"Cheat events",
+	"Game events"
+};
+
+const char* events[] =
+{
+	"Player hits",
+	"Items",
+	"Bomb",
+	"Spread misses",
+	"Occlusion misses",
+	"Misses by unknown reasons"
+};
+
+const char* weather[] =
+{
+	"Off",
+	"Rain"
+
+};
+
+const char* grenades[] =
+{
+	"Grenades",
+	"Armor",
+	"Taser",
+	"Defuser"
+};
+
+const char* fakelags[] =
+{
+	"Static",
+	"Fluctuate"
+};
+
+const char* lagstrigger[] =
+{
+	"Slow walk",
+	"Move",
+	"Air",
+	"Peek"
+};
+
+const char* sounds[] =
+{
+	"None",
+	"leencheat",
+	"Bell",
+	"Flick"
+};
+
+const char* player_model_t[] =
+{
+	"None",
+	"Getaway Sally | The Professionals",
+	"Number K | The Professionals",
+	"Little Kev | The Professionals",
+	"Safecracker Voltzmann | The Professionals",
+	"Bloody Darryl The Strapped | The Professionals",
+	"Sir Bloody Loudmouth Darryl | The Professionals",
+	"Sir Bloody Darryl Royale | The Professionals",
+	"Sir Bloody Skullhead Darryl | The Professionals",
+	"Sir Bloody Silent Darryl | The Professionals",
+	"Sir Bloody Miami Darryl | The Professionals",
+	"Street Soldier | Phoenix",
+	"Soldier | Phoenix",
+	"Slingshot | Phoenix",
+	"Enforcer | Phoenix",
+	"Mr. Muhlik | Elite Crew",
+	"Prof. Shahmat | Elite Crew",
+	"Osiris | Elite Crew",
+	"Ground Rebel | Elite Crew",
+	"The Elite Mr. Muhlik | Elite Crew",
+	"Trapper | Guerrilla Warfare",
+	"Trapper Aggressor | Guerrilla Warfare",
+	"Vypa Sista of the Revolution | Guerrilla Warfare",
+	"Col. Mangos Dabisi | Guerrilla Warfare",
+	"Arno The Overgrown | Guerrilla Warfare",
+	"'Medium Rare' Crasswater | Guerrilla Warfare",
+	"Crasswater The Forgotten | Guerrilla Warfare",
+	"Elite Trapper Solman | Guerrilla Warfare",
+	"'The Doctor' Romanov | Sabre",
+	"Blackwolf | Sabre",
+	"Maximus | Sabre",
+	"Dragomir | Sabre",
+	"Rezan The Ready | Sabre",
+	"Rezan the Redshirt | Sabre",
+	"Dragomir | Sabre Footsoldier",
+};
+
+const char* player_model_ct[] =
+{
+	"None",
+	"Cmdr. Davida 'Goggles' Fernandez | SEAL Frogman",
+	"Cmdr. Frank 'Wet Sox' Baroud | SEAL Frogman",
+	"Lieutenant Rex Krikey | SEAL Frogman",
+	"Michael Syfers | FBI Sniper",
+	"Operator | FBI SWAT",
+	"Special Agent Ava | FBI",
+	"Markus Delrow | FBI HRT",
+	"Sous-Lieutenant Medic | Gendarmerie Nationale",
+	"Chem-Haz Capitaine | Gendarmerie Nationale",
+	"Chef d'Escadron Rouchard | Gendarmerie Nationale",
+	"Aspirant | Gendarmerie Nationale",
+	"Officer Jacques Beltram | Gendarmerie Nationale",
+	"D Squadron Officer | NZSAS",
+	"B Squadron Officer | SAS",
+	"Seal Team 6 Soldier | NSWC SEAL",
+	"Buckshot | NSWC SEAL",
+	"Lt. Commander Ricksaw | NSWC SEAL",
+	"'Blueberries' Buckshot | NSWC SEAL",
+	"3rd Commando Company | KSK",
+	"'Two Times' McCoy | TACP Cavalry",
+	"''Two Times' McCoy | USAF TACP",
+	"'Primeiro Tenente | Brazilian 1st Battalion",
+	"'Cmdr. Mae 'Dead Cold' Jamison | SWAT",
+	"'1st Lieutenant Farlow | SWAT",
+	"'John 'Van Healen' Kask | SWAT",
+	"'Bio-Haz Specialist | SWAT",
+	"'Sergeant Bombson | SWAT",
+	"'Chem-Haz Specialist | SWAT",
+	"'Lieutenant 'Tree Hugger' Farlow | SWAT"
+};
+
